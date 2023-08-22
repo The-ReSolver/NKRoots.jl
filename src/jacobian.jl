54697f05e592e17785aeafd820e9b783067f3066
Thomas Burton <tburton5572@gmail.com>
2023-08-22 11:12:10 +0100

    altered step size for jacobian FD

--- src/jacobian.jl
+++ src/jacobian.jl
@@ -38,12 +38,17 @@ function Base.:*(op::JacobianAD{X}, y::X) where {X}
     return out
 end
 
+# type interface to allow dispatch on method to determine FD step size
+abstract type EpsilonMethod end
+struct Average <: EpsilonMethod end
+struct Nitsol <: EpsilonMethod end
+
 # Object to approximate the action of the jacobian of a function
 # using a finite difference quotient.
 # Assumes F obeys :
 # F(out, x)
 # and overwrites `out` with F(x), returning out
-struct JacobianFD{X, FT}
+struct JacobianFD{X, FT, STEP}
       F::FT          # operator
       x::X           # point at which the derivative is taken
      Fx::X           # value of the function at `x`
@@ -52,8 +57,12 @@ struct JacobianFD{X, FT}
 end
 
 # outer constructor
-jacobianFD(F, x, ϵ::Float64=1e-6) =
-    JacobianFD(F, x, F(similar(x), x), (similar(x), similar(x)), ϵ)
+jacobianFD(F, x, ϵ::Float64=1e-6; step_size::EpsilonMethod=Average()) =
+    JacobianFD{typeof(x), typeof(F), typeof(step_size)}(F, x, F(similar(x), x), (similar(x), similar(x)), ϵ)
+
+# methods to compute FD step size (see page 363 of https://www.sciencedirect.com/science/article/pii/S0021999103004340)
+_get_step(op::JacobianFD{<:Any, <:Any, Average}, y) = (1/(length(op.x)*norm(y)))*sum(op.ϵ.*abs.(op.x)) + op.ϵ
+_get_step(op::JacobianFD{<:Any, <:Any, Nitsol}, y) = sqrt((1 + norm(op.x))*eps(Float64))/norm(y)
 
 # update Fx assuming x has been modified outside
 function update!(j::JacobianFD{X}) where {X}
@@ -62,7 +71,7 @@ function update!(j::JacobianFD{X}) where {X}
 end
 
 # matrix vector product function required by GMRES
-function Base.:*(op::JacobianFD{X}, y::X) where {X}
+function Base.:*(op::JacobianFD{X, <:Any, STEP}, y::X) where {X, STEP}
     # create output
     out = similar(y)
 
@@ -71,8 +80,7 @@ function Base.:*(op::JacobianFD{X}, y::X) where {X}
     Fx_plus_y = op.tmp[2]
             x = op.x
            Fx = op.Fx
-            ϵ = op.ϵ
-            δ = ϵ.*norm(y)/norm(x) # choice of δ given on page 10 of: https://arxiv.org/pdf/1908.06730.pdf
+            δ = _get_step(op, y)
 
     # if perturbation is zero set output to zero
     if norm(y) == 0
@@ -90,4 +98,3 @@ function Base.:*(op::JacobianFD{X}, y::X) where {X}
 
     return out
 end
-
